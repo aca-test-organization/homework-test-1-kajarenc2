9280e8740fb93c87ffb2c08c7ab9456b0abb31f9
kajarenc <kajarenc@gmail.com>
2017-03-06 04:39:44 +0400

    bad solution for travis flow

--- summa.py
+++ summa.py
@@ -1,11 +1,11 @@
 # implement function that sum two numbers
 # implement function that multiply two numbers
+import random
 
 
 def summa(x, y):
-    return x + y
+    return x + y + int(random.getrandbits(1))
 
 
 def product(x, y):
-    return x * y
-
+    return x * y + 1
